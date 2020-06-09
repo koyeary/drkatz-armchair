92f31cbb9074794b408ebc91ca3aff4dafbbd01e
Kat Yeary <katyeary@gmail.com>
2020-06-09 13:32:58 -0700

    inquirer prompts complete

--- Develop/app.js
+++ Develop/app.js
@@ -1,16 +1,73 @@
-const Manager = require("./lib/Manager");
+/* const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Intern = require("./lib/Intern"); */
 const inquirer = require("inquirer");
-const path = require("path");
+/* const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+const render = require("./lib/htmlRenderer"); */
 
+function promptUser() {
+    return inquirer
+        .prompt([
+            {
+                message: "Employee name:",
+                name: "name"
+            },
+            {
+                message: "Employee ID:",
+                name: "id"
+            },
+            {
+                message: "Employee Email:",
+                name: "email"
+              },
+            {
+                type: "rawlist",
+                name: "role",
+                message: "This employee is an:",
+                choices: ["Engineer",
+                        "Intern",
+                        "Manager"]
+            }
+        ])
+        .then(function(answers) {
+            const role = answers.role;
+            if (role === "Engineer") {
+                return inquirer
+                    .prompt(
+                       {    
+                            name: "gitHub",
+                            message: "GitHub username:"
+                       }
+                        
+                    )
+            } else if (role === "Intern") {
+                return inquirer
+                .prompt(
+                   {    
+                        name: "school",
+                        message: "School:"
+                   }
+                    
+                )
+            } else if (role === "Manager") {
+                return inquirer
+                .prompt(
+                   {    
+                        name: "officeNumber",
+                        message: "Office Number:"
+                   }
+                    
+                )
+                 }
+        })
+}
 
+promptUser();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
