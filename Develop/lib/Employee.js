a3b8e4748042c6fa7f1e7064c91164c6d28ed020
Kat Yeary <katyeary@gmail.com>
2020-06-09 20:46:04 -0700

    passes all tests

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,14 +1,25 @@
-module.exports = class Employee  {
-    constructor(name, id, email, role) {
+const inquirer = require("inquirer");
+
+class Employee {
+
+    constructor(name, id, email/* , role */) {
         this.name = name;
         this.id = id;
         this.email = email;
-        this.role = role;
-//set id and email via constructor argument  
-  /*   getName()
-    getId()
-    getEmail()
-    getRole()  *///Returns 'Employee'
+       /*  this.role = role; */
     }
-}       
 
+    getName() {
+      return this.name;
+    }
+    getId() {
+      return this.id;
+    }
+    getEmail() {
+      return this.email;
+    }
+    getRole() {
+      return "Employee";
+    }
+  }
+module.exports = Employee;
